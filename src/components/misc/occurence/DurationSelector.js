c62311df965361d671b9d23ca7db5edd9127175b
Clement AUGER <clem.auger@hotmail.fr>
2022-12-21 15:22:36 +0100

    disable user select

--- src/components/misc/occurence/DurationSelector.js
+++ src/components/misc/occurence/DurationSelector.js
@@ -35,6 +35,13 @@ const ChangeButton = styled.div`
   font-size: ${(props) => (props.large ? 1 : 0.75)}rem;
   background-color: transparent;
   color: ${(props) => props.theme.colors.background};
+
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently supported by Chrome, Edge, Opera and Firefox */
 `
 
 export default function DurationSelector(props) {
