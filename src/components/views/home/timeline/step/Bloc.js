8f63c88f888527d9e88fb3ccc82061c66d12cdea
Florian <hi@florianpanchout.com>
2022-12-31 09:42:52 +0100

    Add real height + opacity gradient to blocs

--- src/components/views/home/timeline/step/Bloc.js
+++ src/components/views/home/timeline/step/Bloc.js
@@ -6,18 +6,10 @@ import DataContext from 'components/providers/DataProvider'
 const Wrapper = styled.div`
   position: relative;
   z-index: 10;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   width: 100%;
-  height: ${(props) => props.power * 0.01}rem;
-  font-size: 0.75rem;
-  font-weight: bold;
-  text-transform: uppercase;
-  color: ${(props) => props.theme.colors.background};
+  min-height: ${(props) => props.power * 0.01}rem;
   background-color: ${(props) =>
     props.theme.colors[props.peak ? 'error' : 'main']};
-  overflow: hidden;
   opacity: ${(props) => (props.discret ? 0.3 : 1)};
   transition: opacity ${(props) => props.discret && '200ms'} ease-out;
   cursor: pointer;
@@ -34,10 +26,9 @@ export default function Bloc(props) {
           ? active?.occurence !== props.bloc.index
           : hover && hover.occurence !== props.bloc.index
       }
-      blocHeight={props.blocHeight}
       onMouseEnter={() => setHover({ occurence: props.bloc.index })}
       onMouseLeave={() => setHover(null)}
       onClick={() => setActive({ occurence: props.bloc.index })}
-    ></Wrapper>
+    />
   )
 }
