6f491d82fd42336cc8a77b10858ee0027ee3d457
Florian <hi@florianpanchout.com>
2023-01-12 10:56:28 +0100

    add airtable iframe

--- src/components/views/home/ExportSimulation.js
+++ src/components/views/home/ExportSimulation.js
@@ -1,35 +1,21 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { useRouter } from 'next/router'
 
-import DataContext from 'components/providers/DataProvider'
+import ModalContext from 'components/providers/ModalProvider'
 import Button from 'components/base/Button'
 
 export default function ExportSimulation() {
-  const { occurences } = useContext(DataContext)
-  const router = useRouter()
-  const [display, setDisplay] = useState(false)
-  useEffect(() => {
-    setDisplay(router.asPath.includes('export'))
-  }, [router])
+  const { setContact } = useContext(ModalContext)
 
-  return display ? (
+  return (
     <Button.Wrapper>
       <Button
         onClick={() => {
-          let element = document.createElement('a')
-          element.setAttribute(
-            'href',
-            'data:application/json;charset=utf-8,' + JSON.stringify(occurences)
-          )
-          element.setAttribute('download', 'simulation-wattris.json')
-          element.style.display = 'none'
-          document.body.appendChild(element)
-          element.click()
-          document.body.removeChild(element)
+          setContact(true)
         }}
       >
-        Exporter ma simulation
+        Je donne mon avis
       </Button>
     </Button.Wrapper>
-  ) : null
+  )
 }
